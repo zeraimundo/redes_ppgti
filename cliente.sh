d163676fe238751b6e326f54026927e219acd57c
JOSE RAIMUNDO BEZERRA DE CARVALHO <zeraimundo@gmail.com>
2024-06-13 16:45:14 -0300

    Create cliente.sh

---
+++ cliente.sh
@@ -0,0 +1,17 @@
+for i in {1..30}
+do
+    echo "Execution $i"
+
+    # Executa os comandos do script 2
+    sudo tcpdump -n -i enp7s0 udp port 8000 or udp port 8001 -w receive_$i.pcap &
+    tcpdump_pid=$!
+    ffplay -autoexit rtsp://10.0.4.10:8554/mystream 
+
+    # Espera 61 segundos
+    sleep 3
+
+    # Mata o processo tcpdump
+    sudo kill $tcpdump_pid
+
+    echo "Execution $i completed"
+done
