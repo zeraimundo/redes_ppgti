2bb8e0835c5247726cee1b512617d3e9c49c30d1
JOSE RAIMUNDO BEZERRA DE CARVALHO <zeraimundo@gmail.com>
2024-06-14 11:37:49 -0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -7,13 +7,17 @@ do
     # Executa os comandos do script 1
     sudo tcpdump -n -i enp7s0 udp port 8000 or udp port 8001 -w sender_$i.pcap &
     tcpdump_pid=$!
-    ffmpeg -re -i bbb_1minuto.mp4 -f rtsp rtsp://localhost:8554/mystream 
+    ffmpeg -re -i bbb_1minuto.mp4 -vf scale=1280:720 -an -f rtsp rtsp://localhost:8554/mystream 
 
-    # Espera 61 segundos
+    # Espera 3 segundos
     sleep 3
 
     # Mata o processo tcpdump
     sudo kill $tcpdump_pid
 
+    # Certifica-se de que todos os processos tcpdump estão mortos
+    sudo pkill -f tcpdump
+
     echo "Execution $i completed"
 done
+
