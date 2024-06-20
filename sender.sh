2e1d5d15b6c13f56b931bb9e34513fa2d691aba9
JOSE RAIMUNDO BEZERRA DE CARVALHO <zeraimundo@gmail.com>
2024-06-20 12:16:59 -0300

    Update sender.sh

--- sender.sh
+++ sender.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+    
     # Executa os comandos do script 1
     
     sudo tcpdump -n -i enp7s0 -w sender_$1.pcap &
     tcpdump_pid=$!
     
-    ffmpeg -re -i video.mp4 -vf scale=1280:720 -b:v 80M  -an -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream && sleep 2 
-#    ffmpeg -re -i video.mp4 -vf scale=1920:1080 -b:v 80M  -an -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream && sleep 2 
-#    ffmpeg -re -i video.mp4 -vf scale=3840:2160 -b:v 80M  -an -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream && sleep 2 
+    ffmpeg -re -i video.mp4 -vf scale=1280:720 -b:v 80M  -an -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream
+#    ffmpeg -re -i video.mp4 -vf scale=1920:1080 -b:v 80M  -an -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream
+#    ffmpeg -re -i video.mp4 -vf scale=3840:2160 -b:v 80M  -an -f rtsp -rtsp_transport tcp rtsp://localhost:8554/mystream
     sleep 3
 
     # Mata o processo tcpdump
